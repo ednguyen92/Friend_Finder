0b036507a71d983b34f61b5ae6cb89d5a28aedc4
ednguyen <ednguyen92@gmail.com>
2018-05-24 18:23:13 -0700

    .

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendsArr = [
+  {
+    "name": "Edward Nguyen",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "scores": [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+]
+
+module.exports = friendsArr;
